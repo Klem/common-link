84de7fb191c82ac7567ce0956d8fbaca9677a42f
Klem <klem86@protonmail.com>
2026-04-27 11:24:31 +0200

    Backend: MoneriumController

---
+++ api/src/main/kotlin/org/commonlink/dto/MoneriumAuthUrlDto.kt
@@ -0,0 +1,4 @@
+package org.commonlink.dto
+
+/** Response DTO for GET /api/monerium/auth-url. */
+data class MoneriumAuthUrlDto(val authUrl: String)
---
+++ api/src/main/kotlin/org/commonlink/dto/MoneriumStatusDto.kt
@@ -0,0 +1,4 @@
+package org.commonlink.dto
+
+/** Response DTO for GET /api/monerium/status. */
+data class MoneriumStatusDto(val connected: Boolean)
